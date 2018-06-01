f1283e219e6d97c44e7a57ba01fb904387384180
Renz Mergenio <zner.mergenio@gmail.com>
2018-06-01 13:14:43 -0800

    Program blade clearing white spaces

--- resources/views/pages/program/program.blade.php
+++ resources/views/pages/program/program.blade.php
@@ -100,11 +100,6 @@
                 </div>
             </div>
         </div>
-<<<<<<< HEAD
-=======
-
-
->>>>>>> afdf97f6cdc97c4b716d930ff36b5973897e9cac
         <div class="modal fade" id="student-modal" tabindex="-1" role="dialog">
             <div class="modal-dialog modal-lg" role="document">
                 <div class="modal-content">
@@ -138,7 +133,6 @@
                                                 </td>
                                                 <td class="text-center text-bold text-green">@{{ student.application_id }}</td>
                                             </tr>
-<<<<<<< HEAD
                                             </tbody>
                                         </table>
                                     </section>
