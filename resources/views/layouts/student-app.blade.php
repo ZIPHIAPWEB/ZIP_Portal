374d888ff2b679d423bae522c956dac5c8481b75
Renz Mergenio <zner.mergenio@gmail.com>
2019-03-21 17:03:05 +0800

    Footer Date

--- resources/views/layouts/student-app.blade.php
+++ resources/views/layouts/student-app.blade.php
@@ -57,8 +57,7 @@
             <div class="pull-right hidden-xs">
                 <b>Version</b> 2.4.0
             </div>
-            <strong>Copyright &copy; 2014-2016 <a href="https://adminlte.io">Almsaeed Studio</a>.</strong> All rights
-            reserved.
+            All rights reserved {{ date('Y') }}
         </div>
         <!-- /.container -->
     </footer>
