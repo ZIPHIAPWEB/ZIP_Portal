7213e51465b6909dbda4bf7fc1cdb8ce0bdecf2b
Renz Mergenio <zner.mergenio@gmail.com>
2019-05-27 15:29:55 +0800

    Bugfix - Request not found

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Http\Request;
 
 class RegisterController extends Controller
 {
