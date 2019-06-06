15932eec29328dab8a7f052ac7cb0ee3a43d01fb
Renz Mergenio <zner.mergenio@gmail.com>
2019-06-06 15:20:31 +0800

    payment validation

--- resources/views/pages/requirement/payment.blade.php
+++ resources/views/pages/requirement/payment.blade.php
@@ -349,7 +349,7 @@
                     let formData = new FormData();
                     formData.append('bank_code', this.payment.bank_code);
                     formData.append('reference_no', this.payment.ref_no);
-                    formData.append('date_deposity', this.payment.date);
+                    formData.append('date_deposit', this.payment.date);
                     formData.append('bank_account', this.payment.bank_account);
                     formData.append('amount', this.payment.amount);
                     formData.append('file', this.payment.file);
