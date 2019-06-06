6328123623194b74e3d43619b046f22be061f77c
Renz Mergenio <zner.mergenio@gmail.com>
2019-06-06 15:53:00 +0800

    superadmin payment delete

--- app/Http/Controllers/PaymentRequirementController.php
+++ app/Http/Controllers/PaymentRequirementController.php
@@ -8,6 +8,7 @@ use App\Repositories\PaymentRequirement\PaymentRequirementRepository;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class PaymentRequirementController extends Controller
 {
