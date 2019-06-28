ac2125232c66afc028547a3f8e811a26270b154a
Renz Mergenio <zner.mergenio@gmail.com>
2019-06-28 10:01:48 +0800

    Chat box title

--- resources/views/pages/chatbox.blade.php
+++ resources/views/pages/chatbox.blade.php
@@ -10,7 +10,7 @@
             <div class="box box-primary">
                 <div class="box-header with-border">
                     <div class="box-title">
-                        sdlfjsdkfj
+                        Students
                     </div>
 
                     <div class="box-tools pull-right">
