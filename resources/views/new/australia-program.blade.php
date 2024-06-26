3b69d1a7b1e12b53ff28055291cad13ff5bf09f4
Renz Mergenio <xurpas@Users-MacBook-Pro.local>
2024-06-26 21:13:56 +0800

    refactor: added clickable card with modals

--- resources/views/new/australia-program.blade.php
+++ resources/views/new/australia-program.blade.php
@@ -1,10 +1,21 @@
-<!DOCTYPE html>
-<html data-bs-theme="light" lang="en">
+@extends('new.layouts.app')
 
-@include('new.partials.head')
+@section('meta')
+    <meta name="title" content="Australia Programs | ZIP Travel Philippines">
+    <meta name="description" content="Join our Australia programs and explore Australia's wonders, unlock your potential, and seize opportunities for personal and professional growth.">
 
-<body>
-    @include('new.partials.navbar')
+    <meta property="og:title" content="Australia Programs | ZIP Travel Philippines" />
+    <meta property="og:description" content="Join our Australia programs and explore Australia's wonders, unlock your potential, and seize opportunities for personal and professional growth.">
+    <meta property="og:type" content="article" />
+    <meta property="og:url" content="https://ziptravel.com.ph/program-australia" />
+
+    <meta name="twitter:title" content="Australia Programs | ZIP Travel Philippines">
+    <meta name="twitter:description" content="Join our Australia programs and explore Australia's wonders, unlock your potential, and seize opportunities for personal and professional growth.">
+@endsection
+
+@section('title', 'Australia Programs | ZIP Travel Philippines')
+
+@section('content')
     <div id="heading" class="heading">
         <video style="position: absolute;z-index: 0;min-height: 100%;min-width: 100%;object-fit: fill;overflow: hidden;" muted autoplay loop>
             <source src="{{ asset('assets_v3/videos/AUSTRALIA_HEADER.mp4') }}" type="video/mp4">
@@ -24,14 +35,14 @@
             </div>
             <div class="row" style="margin-top: 60px;">
                 <div class="col">
-                    <div style="width: 100%;height: 434px;border-radius: 40px;box-shadow: 10px 13px 20px 0px var(--bs-secondary-color);"><img style="height: 352px;width: 100%;object-fit: cover;" src="assets_v3/img/Rectangle%2011%20(5).png">
+                    <div data-bs-toggle="modal" data-bs-target="#studyWorkModal" style="cursor: pointer; width: 100%;height: 434px;border-radius: 40px;box-shadow: 10px 13px 20px 0px var(--bs-secondary-color);"><img style="height: 352px;width: 100%;object-fit: cover;" src="assets_v3/img/Rectangle%2011%20(5).png">
                         <div class="d-flex justify-content-center align-items-center" style="height: 82px;width: 100%;">
                             <p style="font-family: 'Outfit';font-weight: bold;font-size: 20px;">Study and Work</p>
                         </div>
                     </div>
                 </div>
                 <div class="col">
-                    <div style="width: 100%;height: 434px;border-radius: 40px;box-shadow: 10px 13px 20px 0px var(--bs-secondary-color);"><img style="height: 352px;width: 100%;object-fit: cover;" src="assets_v3/img/Rectangle%2020.png">
+                    <div data-bs-toggle="modal" data-bs-target="#hospitalityTraineeModal" style="cursor: pointer; width: 100%;height: 434px;border-radius: 40px;box-shadow: 10px 13px 20px 0px var(--bs-secondary-color);"><img style="height: 352px;width: 100%;object-fit: cover;" src="assets_v3/img/Rectangle%2020.png">
                         <div class="d-flex justify-content-center align-items-center" style="height: 82px;width: 100%;">
                             <p style="font-family: 'Outfit';font-weight: bold;font-size: 20px;">Hospitality Trainee</p>
                         </div>
@@ -91,8 +102,77 @@
             <a href="https://ziptravel.com.ph/online-registration" target="_blank" class="btn btn-primary" role="button" style="width: 173px;height: 53px;vertical-align: text-bottom;text-align: center;background: #510A0A;padding: 10px;font-size: 20px;font-family: 'Outfit';margin-top: 12px;border-radius: 40px;border: 0;">APPLY NOW!</a>
         </div>
     </section><!-- End: apply-now -->
-    @include('new.partials.footer')
-    @include('new.partials.scripts')
-</body>
 
-</html>
+    <!-- Modal -->
+    <div class="modal fade" id="studyWorkModal" tabindex="-1" aria-labelledby="studyWorkModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" style="padding: 2rem;">
+                    <div>
+                        <h3 style="font-family:'Outfit'; font-size: 20px;">STUDY AND WORK</h3>
+                        <span style="font-family:'Inter'; font-size: 18px;">1 to 4 years (depending on the course and level of study)</span>
+                        <p style="font-family: 'Inter'; font-size: 18px; margin-top: 1rem;">
+                            Designed for international students who wish to pursue their education while gaining valuable work experience in Australia. The program is under the Subclass 500 Visa type, allowing a participant to stay up to 5 years as a full-time student.
+                        </p>
+                        <ul style="font-size: 18px; font-family: 'Inter'; margin-top: 0.8rem;">
+                            <li>Open to K-12 graduates, college graduates, and professionals</li>
+                            <li>Enhance your career prospect</li>
+                            <li>Enroll in globally-ranked institutions such as The University of Notre Dame Australia, and Deakin University, among others</li>
+                            <li>Work part-time while studying or full-time during official school breaks</li>
+                            <li>Opportunity to apply for a Temporary Graduate Visa after graduation</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 style="font-family:'Outfit'; font-size: 20px;">ELIGIBILITY</h3>
+                        <ul style="font-size: 18px; font-family: 'Inter'; margin-top: 0.8rem;">
+                            <li>Must be at least a high school or college graduate</li>
+                            <li>Certificates and diplomas take 1 to 2 years, while undergraduate and graduate degrees take 3 to 4 years</li>
+                            <li>Sufficiently proficient in the English language, able to interact in an English-speaking environment</li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Modal -->
+    <div class="modal fade" id="hospitalityTraineeModal" tabindex="-1" aria-labelledby="hospitality-trainee" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body" style="padding: 2rem;">
+                    <div>
+                        <h3 style="font-family:'Outfit'; font-size: 20px;">HOSPITALITY TRAINEE</h3>
+                        <span style="font-family:'Inter'; font-size: 18px;">12 to 18 months</span>
+                        <p style="font-family: 'Inter'; font-size: 18px; margin-top: 1rem;">
+                            Intended for candidates aspiring to work their way up the ranks and are committed to advancing their careers. They must at least demonstrate passion, dedication, and previous work experience.
+                            <br><br>
+                            Qualified candidates will be issued the 407 Training Visa, an initiative by the Australian Government and managed by the Department of Home Affairs and the approved sponsor. It is a temporary visa for participants undertaking workplace-based training to improve their skills.
+                        </p>
+                        <ul style="font-size: 18px; font-family: 'Inter'; margin-top: 0.8rem;">
+                            <li>Hone your skills under the tutelage of some of the top cooks and chefs</li>
+                            <li>Gain exposure to international culinary practices and Australian regional cuisine</li>
+                            <li>Gateway to culinary culture and customs followed by world-renowned chefs</li>
+                            <li>Make meaningful relationships with contacts</li>
+                            <li>Earn a stipend to support your stay in the country</li>
+                            <li>Work in cities and regional locations across Australia</li>
+                            <li>With the possibility to extend stay for up to 2 years</li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h3 style="font-family:'Outfit'; font-size: 20px;">ELIGIBILITY</h3>
+                        <ul style="font-size: 18px; font-family: 'Inter'; margin-top: 0.8rem;">
+                            <li>18 to 28 years old</li>
+                            <li>A graduate of hospitality or culinary course for not more than 18 months</li>
+                            <li>Must have a minimum of 12 months of ongoing work experience in a 4-star and above hotel/restaurant</li>
+                            <li>Must be an effective communicator in an English-speaking environment</li>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
