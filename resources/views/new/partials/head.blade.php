8f76cbff95026100f696773105b0c2259ebb9ca7
Renz Mergenio <zner.mergenio@gmail.com>
2024-08-29 21:47:27 +0800

    fix: navbar small screen background | error on css file

--- resources/views/new/partials/head.blade.php
+++ resources/views/new/partials/head.blade.php
@@ -5,10 +5,10 @@
     <meta name="keywords" content="Zip Travel, Cultural Exchange Program, J1 Visa, International Internship, Trainee, Culinary, Front Office, F&B, Internship, BridgeUSA, USA, Canada, Spain, Asia, Australia, Exchange Program, Summer Work Travel, Teacher Program, Study Abroad">
     @yield('meta')
     <title>@yield('title')</title>
-    <link rel="stylesheet" href="assets_v3/bootstrap/css/bootstrap.min.css">
-    <link rel="stylesheet" href="assets_v3/fonts/fontawesome-all.min.css">
-    <link rel="stylesheet" href="assets_v3/css/styles.min.css">
-    <link rel="stylesheet" href="assets_v3/css/custom.css">
+    <link rel="stylesheet" href="{{ asset('assets_v3/bootstrap/css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets_v3/fonts/fontawesome-all.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets_v3/css/styles.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets_v3/css/custom.css') }}">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap" rel="stylesheet">
--- resources/views/new/partials/navbar.blade.php
+++ resources/views/new/partials/navbar.blade.php
@@ -1,4 +1,4 @@
-<nav id="navbar" class="navbar navbar-expand-md fixed-top" style="padding: 50px 0;">
+<nav id="navbar" class="navbar navbar-expand-lg fixed-top" style="padding: 50px 0;">
     <div class="container">
         <a class="navbar-brand" href="/">
             <img src="assets_v3/img/Zip%20Logo%20High%20Reso%20-%20Circle_big%20-%20Inner-02b%20W%201.png">
@@ -8,7 +8,7 @@
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse text-end" id="navcol-1">
-            <ul class="navbar-nav ms-auto" style="gap: 20px;font-size: 20px;font-family: Outfit, sans-serif;">
+            <ul class="nav-sm-bg navbar-nav ms-auto" style="gap: 20px;font-size: 20px;font-family: Outfit, sans-serif;">
                 <li class="nav-item"><a class="nav-link active" href="/">Home</a></li>
                 <li class="nav-item"><a class="nav-link" href="/about-us">About Us</a></li>
                 <li class="nav-item">
