61d5e0689f1e2caae0a3e45c4c88f2347fb01128
Renz Mergenio <zner.mergenio@gmail.com>
2024-09-20 10:36:41 +0800

    fix: update social streams header section

--- resources/views/new/social-streams.blade.php
+++ resources/views/new/social-streams.blade.php
@@ -5,15 +5,12 @@
 
 <body>
     @include('new.partials.navbar')
-    <div id="heading" class="heading">
-        <video class="heading__video" muted autoplay loop>
-            <source src="{{ asset('assets_v3/videos/ABOUT US_HEADER.mp4') }}" type="video/mp4">
-        </video>
-        <div class="heading__content container d-flex d-xxl-flex flex-column justify-content-center align-items-center justify-content-sm-center justify-content-md-end align-items-lg-start h-100">
+    <div id="heading" class="heading" style="height: 50vh; background: url('/assets_v3/img/social_stream.png');">
+        <div class="heading__content container d-flex d-xxl-flex flex-column justify-content-center align-items-center justify-content-sm-center justify-content-md-end align-items-lg-start" style="height: 50vh;">
             <div class="d-flex d-sm-flex d-md-flex d-xxl-flex flex-column justify-content-sm-center align-items-sm-center align-items-md-start justify-content-xxl-start" style="color: #FFFFFF;margin-bottom: 43px;">
                 <h1 class="text-center heading__content-title">SOCIAL STREAMS</h1>
                 <p class="text-center text-sm-center text-md-start heading__content-description">
-                ZIP Travel is an international company that has been at the forefront of providing educational and cultural exchange programs to students and young professionals since its establishment in Europe in 1993.
+                    Follow us on our social media for the latest updates, news, and exclusive content!
                 </p>
             </div>
         </div>
